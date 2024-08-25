b860e8e5976812b207f6e83e1efb742677779611
ALGOMARBLER\algom <YW30520@gmail.com>
2024-08-25 20:26:55 +1200

    made light and dark mode

--- app/src/main/java/com/example/goldclickerv2/ui/theme/components.kt
+++ app/src/main/java/com/example/goldclickerv2/ui/theme/components.kt
@@ -17,52 +17,49 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.goldclickerv2.R
 
+
 @Composable
 fun MainBox(goldCount: MutableIntState, perClick: MutableIntState) {
+
+    val modeMultiplier = remember { mutableIntStateOf(1) } // 1 = light mode, -1 = dark mode
+    val backgroundColor = if (modeMultiplier.intValue == 1) Color.White else Color.Black
+    val textColor = if (modeMultiplier.intValue == 1) Color.Black else Color.White
+    val secondaryBackgroundColor = if (modeMultiplier.intValue == 1) Color.LightGray else Color.DarkGray
+
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.White)
+            .background(color = backgroundColor)
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier
-                    .background(color = Color.LightGray)
+                    .background(color = secondaryBackgroundColor)
                     .padding(10.dp)
                     .fillMaxWidth()
             ) {
                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
                     GoldClicker(goldCount, perClick)
-                    TextDescription(goldCount.intValue, perClick.intValue)
+                    TextDescription(goldCount.intValue, perClick.intValue, textColor, backgroundColor, modeMultiplier)
                 }
             }
             Spacer(modifier = Modifier.height(24.dp))
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier
-                    .background(color = Color.LightGray)
+                    .background(color = secondaryBackgroundColor)
                     .padding(10.dp)
                     .fillMaxWidth()
-                    .weight(1f) // Makes the shop section fill available space
             ) {
                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(text = "SHOP", fontSize = 24.sp)
-                    Buttons(goldCount, perClick)
+                    Text(text = "SHOP", fontSize = 24.sp, color = textColor)
+                    Buttons(goldCount, perClick, modeMultiplier.intValue, backgroundColor )
                 }
             }
         }
     }
-}
-
-@Composable
-fun GoldImage() {
-    Image(
-        painter = painterResource(id = R.drawable.goldcoin),
-        contentDescription = null,
-        modifier = Modifier.size(200.dp)
-    )
 }
 
 @Composable
@@ -72,40 +69,57 @@ fun GoldClicker(goldCount: MutableIntState, perClick: MutableIntState) {
         colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
         contentPadding = PaddingValues(0.dp)
     ) {
-        GoldImage()
+        Image(
+            painter = painterResource(id = R.drawable.goldcoin),
+            contentDescription = null,
+            modifier = Modifier.size(200.dp)
+        )
     }
 }
 
 @Composable
-fun TextDescription(gold: Int, perClick: Int) {
+fun TextDescription(gold: Int, perClick: Int, textColor: Color, backgroundColor: Color, modeMultiplier: MutableIntState) {
     Text(
         text = "You have $gold coins.",
-        fontSize = 20.sp
+        fontSize = 20.sp,
+        color = textColor
     )
     Text(
-        text = "Per click: $perClick"
+        text = "Per click: $perClick",
+        color = textColor
     )
     Text(
         text = "Click the coin to earn more!",
-        fontSize = 12.sp
+        fontSize = 12.sp,
+        color = textColor
     )
+    Button(
+        onClick = { modeMultiplier.intValue *= -1 },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = backgroundColor,
+            contentColor = textColor
+        ),
+    ) {
+        Text(text = if (modeMultiplier.intValue == 1) "Switch to Dark Mode" else "Switch to Light Mode")
+    }
 }
 
 @Composable
-fun Buttons(goldCount: MutableIntState, perClick: MutableIntState) {
+fun Buttons(goldCount: MutableIntState, perClick: MutableIntState, modeMultiplier: Int, backgroundColor: Color) {
     LazyColumn(
         contentPadding = PaddingValues(vertical = 8.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp),
         modifier = Modifier.fillMaxHeight()
     ) {
         item {
-            ShopRow(15, "Miner", R.drawable.miner_icon, goldCount, perClick, increase = 1)
-            ShopRow(75, "Excavator", R.drawable.excavator_icon, goldCount, perClick, increase = 2)
-            ShopRow(625, "Shovel", R.drawable.shovel_icon, goldCount, perClick, increase = 5)
-            ShopRow(2000, "Cart", R.drawable.cart_icon, goldCount, perClick, increase = 10)
-            ShopRow(12500, "Crane", R.drawable.crane_icon, goldCount, perClick, increase = 20)
-            ShopRow(80000, "Factory", R.drawable.factory_icon, goldCount, perClick, increase = 50)
-            ShopRow(300000, "Gold Mine", R.drawable.goldmine_icon, goldCount, perClick, increase = 100) }
+            ShopRow(15, "Miner", R.drawable.miner_icon, goldCount, perClick, increase = 1, modeMultiplier, backgroundColor)
+            ShopRow(75, "Excavator", R.drawable.excavator_icon, goldCount, perClick, increase = 2, modeMultiplier, backgroundColor)
+            ShopRow(625, "Shovel", R.drawable.shovel_icon, goldCount, perClick, increase = 5, modeMultiplier, backgroundColor)
+            ShopRow(2000, "Cart", R.drawable.cart_icon, goldCount, perClick, increase = 10, modeMultiplier, backgroundColor)
+            ShopRow(12500, "Crane", R.drawable.crane_icon, goldCount, perClick, increase = 20, modeMultiplier, backgroundColor)
+            ShopRow(80000, "Factory", R.drawable.factory_icon, goldCount, perClick, increase = 50, modeMultiplier, backgroundColor)
+            ShopRow(300000, "Gold Mine", R.drawable.goldmine_icon, goldCount, perClick, increase = 100, modeMultiplier, backgroundColor)
+        }
     }
 }
 
@@ -116,17 +130,20 @@ fun ShopRow(
     imageResId: Int,
     goldCount: MutableIntState,
     perClick: MutableIntState,
-    amountOwned: Int = 0,
-    increase: Int
+    increase: Int,
+    modeMultiplier: Int,
+    backgroundColor: Color
 ) {
-    var owned by remember { mutableIntStateOf(amountOwned) }
+    var owned by remember { mutableIntStateOf(0) }
+
+    val textColor = if (modeMultiplier == 1) Color.Black else Color.White
 
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(4.dp)
             .clip(RoundedCornerShape(12.dp))
-            .background(color = Color.DarkGray),
+            .background(color = backgroundColor),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Spacer(modifier = Modifier.width(15.dp))
@@ -144,8 +161,8 @@ fun ShopRow(
                 .weight(1f)
                 .padding(2.dp)
         ) {
-            Text(text = "$name (Per click: +$increase)", fontSize = 14.sp, color = Color.White)
-            Text(text = "You own: $owned", fontSize = 10.sp, color = Color.White)
+            Text(text = "$name (Per click: +$increase)", fontSize = 14.sp, color = textColor)
+            Text(text = "You own: $owned", fontSize = 10.sp, color = textColor)
         }
 
         Button(
@@ -157,8 +174,8 @@ fun ShopRow(
                 }
             },
             colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Gray,
-                contentColor = Color.White
+                containerColor = backgroundColor,
+                contentColor = textColor
             ),
             modifier = Modifier.padding(8.dp)
         ) {
